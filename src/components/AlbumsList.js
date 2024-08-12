f59cc04561d67e8f2fbe442057bf65895b331848
Romaric Zounlome <rzounlom@gmail.com>
2024-08-12 07:07:23 -0400

    feat: create albums api routes

--- src/components/AlbumsList.js
+++ src/components/AlbumsList.js
@@ -1,3 +1,37 @@
+import { useAddAlbumMutation, useFetchAlbumsQuery } from "../store";
+
+import Button from "./Button";
+import ExpandablePanel from "./ExpandablePanel";
+import Skeleton from "./Skeleton";
+
 export default function AlbumsList({ user }) {
-  return <div>Albums for {user.name}</div>;
+  const { data, error, isLoading } = useFetchAlbumsQuery(user);
+  const [addAlbum, results] = useAddAlbumMutation();
+
+  const handleAddAlbum = () => {
+    addAlbum(user);
+  };
+
+  let content;
+
+  if (isLoading) {
+    content = <Skeleton times={3} />;
+  } else if (error) {
+    content = <div>Error: loading Albums</div>;
+  } else {
+    content = data.map((album) => (
+      <ExpandablePanel key={album.id} header={album.title}>
+        List of photos in album
+      </ExpandablePanel>
+    ));
+  }
+  return (
+    <div>
+      <div>Albums for {user.name}</div>
+      <Button onClick={handleAddAlbum} disabled={results.isLoading}>
+        + Add Album
+      </Button>
+      <div>{content}</div>
+    </div>
+  );
 }
--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,20 @@
+import { albumsApi } from "./apis/albumsApi";
 import { configureStore } from "@reduxjs/toolkit";
+import { setupListeners } from "@reduxjs/toolkit/query";
 import { usersReducer } from "./slices/usersSlice";
 
 export const store = configureStore({
   reducer: {
     users: usersReducer,
+    [albumsApi.reducerPath]: albumsApi.reducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(albumsApi.middleware),
 });
+
+setupListeners(store.dispatch);
 
 export * from "./thunks/fetchUsers";
 export * from "./thunks/addUser";
 export * from "./thunks/deleteUser";
+export { useFetchAlbumsQuery, useAddAlbumMutation } from "./apis/albumsApi";
